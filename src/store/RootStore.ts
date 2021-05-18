9c262b5d2b1d3f76bb92f3261b93e272fad618ae
potados99 <potados99@gmail.com>
2021-05-19 01:09:06 +0900

    chore: counter 화면 제거

--- src/store/RootStore.ts
+++ src/store/RootStore.ts
@@ -1,11 +1,9 @@
 import UserStore from '../presentation/features/membership/UserStore';
-import CounterStore from '../presentation/features/counter/CounterStore';
 import CafeteriaStore from '../presentation/features/cafeteria/CafeteriaStore';
 import MembershipStore from '../presentation/features/membership/MembershipStore';
 
 export default class RootStore {
   userStore = new UserStore();
-  counterStore = new CounterStore();
   cafeteriaStore = new CafeteriaStore();
   membershipStore = new MembershipStore();
 }

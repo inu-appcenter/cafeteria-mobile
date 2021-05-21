14afe0512c8203d0937402defc6949818947b05a
potados99 <potados99@gmail.com>
2021-05-21 23:36:27 +0900

    chore: 웹뷰 불필요한 속성 제거!

--- src/presentation/features/support/ServiceManual/index.tsx
+++ src/presentation/features/support/ServiceManual/index.tsx
@@ -3,10 +3,5 @@ import Config from '../../../../common/Config';
 import {WebView} from 'react-native-webview';
 
 export default function ServiceManual() {
-  return (
-    <WebView
-      source={{uri: Config.webPageUrl.serviceManual}}
-      scalesPageToFit={true}
-    />
-  );
+  return <WebView source={{uri: Config.webPageUrl.serviceManual}} />;
 }

bf27202c93e6d270c73aa1f44620a7a2c31bbf28
potados99 <potados99@gmail.com>
2021-05-24 02:03:45 +0900

    fix: 안드로이드 웹뷰 컨텐츠 폭 문제 수정.

--- src/presentation/components/LoadingWebView.tsx
+++ src/presentation/components/LoadingWebView.tsx
@@ -13,6 +13,8 @@ export default function LoadingWebView({uri}: Props) {
   return (
     <WebView
       source={{uri: uri}}
+      injectedJavaScript={fitWidth}
+      scalesPageToFit={false}
       startInLoadingState={true}
       renderLoading={() => (
         <View style={styles.container}>
@@ -32,3 +34,15 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 });
+
+/**
+ * 안드로이드 웹 뷰에서는 viewport 메타 태그가 없으면 컨텐트가 기기 폭에 맞춰지지 않습니다.
+ * 그래서 강제로 집어넣어 줍니다.
+ * 이미 페이지에 해당 태그가 있어도 상관 없나봅니다.
+ */
+const fitWidth = `
+    const meta = document.createElement('meta');
+    meta.setAttribute('content', 'initial-scale=1.0, maximum-scale=1.0'); 
+    meta.setAttribute('name', 'viewport'); 
+    document.getElementsByTagName('head')[0].appendChild(meta);
+`;

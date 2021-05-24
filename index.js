e525d87fee034c4ba2e60a3f2748adf2dbb46690
potados99 <potados99@gmail.com>
2021-05-24 22:53:48 +0900

    chore: 라이센스 헤더 추가

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 /**
- * Entry file!
+ * This file is part of INU Cafeteria.
+ *
+ * Copyright (C) 2021 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import 'reflect-metadata'; // 이 녀석 위치가 참 중요합니다. 제일 처음에 있어야 해요!
--- src/presentation/features/support/Main/index.tsx
+++ src/presentation/features/support/Main/index.tsx
@@ -1,3 +1,22 @@
+/**
+ * This file is part of INU Cafeteria.
+ *
+ * Copyright (C) 2021 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import palette from '../../../res/palette';
 import Feather from 'react-native-vector-icons/Feather';
@@ -7,9 +26,9 @@ import useUserState from '../../../hooks/useUserState';
 import SupportOption from './SupportOption';
 import ItemSeparator from '../../../components/ItemSeparator';
 import ContactsButton from './ContactsButton';
-import {SupportMainNavigation} from '../SupportScreen';
+import {ScrollView, View} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {SupportMainNavigation} from '../SupportScreen';
 
 type Props = {
   navigation: SupportMainNavigation;

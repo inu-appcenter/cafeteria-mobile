94d184e3ed99a5e11fb93f9239bdf4a308cf7e7f
potados99 <potados99@gmail.com>
2021-05-26 21:24:20 +0900

    chore(style): package.json 오름차순 정렬:
    ㅋㅋㅋㅋ

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // node_modules 중에서도 transform이 필요한 의존성은 명시해요!
   transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
 };

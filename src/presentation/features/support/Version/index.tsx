8802f5f66077ed2e3ea8fea2aef3798d35d281b1
Potados <potados99@gmail.com>
2021-05-28 21:20:33 +0900

    feat: version (#2)
    * feat: 버전 확인 버튼 추가
    
    * chore: bundle 버전 표시
    
    * chore: iOS Staging 빌드 설정 추가
    
    * chore: 안드로이드에 releaseStaging 빌드 설정 추가
    
    * fix: 안드로이드 release 계열 빌드에 signingConfig 추가

--- src/presentation/features/support/Version/index.tsx
+++ src/presentation/features/support/Version/index.tsx
@@ -17,15 +17,61 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import React from 'react';
+import useApi from '../../../hooks/useApi';
 import palette from '../../../res/palette';
+import {Button} from 'react-native-paper';
+import codePush from 'react-native-code-push';
 import PackageInfo from '../../../../common/PackageInfo';
-import {Text, View} from 'react-native';
+import PaperPresets from '../../../components/utils/PaperPresets';
+import CheckForUpdates from '../../../../domain/usecases/CheckForUpdates';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
 export default function Version() {
+  const [loading, check] = useApi(() => CheckForUpdates.run());
+
+  const [codePushLabel, setCodePushLabel] = useState('-');
+
+  useEffect(() => {
+    codePush.getUpdateMetadata().then(metadata => {
+      setCodePushLabel(metadata?.label || '-');
+    });
+  }, []);
+
   return (
-    <View style={[palette.centeringContainer, palette.whiteBackground]}>
-      <Text style={palette.textPrimary}>앱 버전: {PackageInfo.version}</Text>
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={palette.textPrimary}>앱 버전: {PackageInfo.version}</Text>
+        <Text style={[palette.textSecondary, styles.marginOnTop]}>
+          Bundle 버전: {codePushLabel}
+        </Text>
+      </View>
+      <Button
+        {...PaperPresets.wideThemedButton}
+        style={styles.button}
+        loading={loading}
+        onPress={check}>
+        업데이트 확인
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...palette.whiteBackground,
+    flexDirection: 'column',
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  marginOnTop: {
+    marginTop: 8,
+  },
+  button: {
+    margin: 12,
+  },
+});

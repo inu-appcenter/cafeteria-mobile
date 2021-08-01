de948042fed79d3929bd40fd3a89fdf05178118b
potados99 <potados99@gmail.com>
2021-08-02 00:57:46 +0900

    feat: iOS 바코드 터치해서 화면 밝아질 때에 fade

--- src/presentation/hooks/useScreenBrightness.ts
+++ src/presentation/hooks/useScreenBrightness.ts
@@ -20,21 +20,50 @@
 // @ts-ignore
 import DeviceBrightness from '@adrianso/react-native-device-brightness';
 import {useState} from 'react';
+import range from '../../common/utils/range';
+import {Platform} from 'react-native';
 
 export default function useScreenBrightness() {
   const [isBright, setIsBright] = useState(false);
-  const [originalBrightness, setOriginalBrightness] = useState(0);
+  const [originalBrightness, saveOriginalBrightness] = useState(0);
 
   const toggleBrightness = async () => {
+    const fadeDuration = Platform.OS === 'ios' ? 0.8 : 0;
+    const currentBrightness = await DeviceBrightness.getBrightnessLevel(); // 항상 1이 아닐 수 있어요.
+
     if (isBright) {
-      await DeviceBrightness.setBrightnessLevel(originalBrightness);
+      await fadeBrightness(currentBrightness, originalBrightness, fadeDuration);
     } else {
-      setOriginalBrightness(await DeviceBrightness.getBrightnessLevel());
-      await DeviceBrightness.setBrightnessLevel(1);
+      saveOriginalBrightness(currentBrightness);
+
+      await fadeBrightness(currentBrightness, 1, fadeDuration);
     }
 
     setIsBright(!isBright);
   };
 
   return [toggleBrightness];
+}
+
+/**
+ * 화면 밝기를 duration동안 from에서 to로 변화시킵니다.
+ *
+ * @param from
+ * @param to
+ * @param duration
+ */
+async function fadeBrightness(from: number, to: number, duration: number) {
+  if (duration === 0) {
+    await DeviceBrightness.setBrightnessLevel(to);
+    return;
+  }
+
+  const fps = 320; // 이정도는 되어야 자연스럽게 느껴집니다.
+  const totalFrames = fps * duration;
+
+  const values = range(from, to, totalFrames);
+
+  for (const value of values) {
+    await DeviceBrightness.setBrightnessLevel(value);
+  }
 }
